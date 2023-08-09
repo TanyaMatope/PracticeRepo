9a25c33628b9e86745f7c2aa2c96e8cdeff53dec
TANYAMATOPE <20231120@CTUCAREER.CO.ZA>
2023-08-09 20:14:46 +0200

    1

---
+++ webapp1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webapp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Our Story";
+
+            return View();
+        }
+
+        public ActionResult Favourites()
+        {
+            ViewBag.Message = "My Favourite Comics";
+
+            return View();
+        }
+
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Location";
+
+            return View();
+        }
+
+        public ActionResult LatestNews()
+        {
+            ViewBag.Message = "Bringing you the latest comics";
+
+            return View();
+        }
+    }
+}
